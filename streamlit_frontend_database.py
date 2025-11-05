1c0f80d606224f7717ce3559b0e50be37781c6d8
Decadent-tech <debosmitac8@gmail.com>
2025-11-05 20:35:47 +0530

    created traces for LANGSMITH Integration for my Chatbot

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -92,8 +92,12 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
     
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
-
+    #CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    CONFIG ={
+        'configurable': {'thread_id': st.session_state['thread_id']},
+        'metadata':{'thread_id': st.session_state['thread_id']},
+        'run_name': 'chatbot_run'
+    }
     with st.chat_message('assistant'):
         ai_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in chatbot.stream(
